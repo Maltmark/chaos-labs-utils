2e45bc74700cf226f785dd3fe28c1b26bfe90d0e
altmark <michala@chaoslabs.xyz>
2023-04-09 17:24:47 +0300

    work

--- scripts/get-borrowers.py
+++ scripts/get-borrowers.py
@@ -2,25 +2,8 @@ import sys
 import requests
 import os
 
-print("Path at terminal when executing this file")
-print(os.getcwd() + "\n")
+path, _ = os.path.split(os.path.realpath(__file__))
 
-print("This file path, relative to os.getcwd()")
-print(__file__ + "\n")
-
-print("This file full path (following symlinks)")
-full_path = os.path.realpath(__file__)
-print(full_path + "\n")
-
-print("This file directory and name")
-path, filename = os.path.split(full_path)
-print(path + ' --> ' + filename + "\n")
-
-print("This file directory only")
-print(os.path.dirname(full_path))
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
 
 BORROWERS_PATH = f"{path}/../src/borrowers.sol"
 
